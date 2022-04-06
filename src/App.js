2221802887356a187a2742f33d3d5537d981e627
Ahmet <a.yalcinkaya@gmail.com>
2022-04-06 10:06:21 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <Link to="/"> React Shopping Cart</Link>
+        <Link to="/admin"> Admin</Link>
       </header>
+      <main></main>
+      <footer>All rights reserved</footer>
     </div>
   );
 }
